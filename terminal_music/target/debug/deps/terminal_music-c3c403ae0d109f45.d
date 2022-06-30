9fe1ac883e3cd08de23fb0f5d2940bdd58f2cc51
Rian-Tan <tan_jun_yu_rian@s2021.ssts.edu.sg>
2022-06-30 20:43:15 +0800

    initialization

---
+++ terminal_music/target/debug/deps/terminal_music-c3c403ae0d109f45.d
@@ -0,0 +1,5 @@
+/home/kali/Terminal-music/terminal_music/target/debug/deps/terminal_music-c3c403ae0d109f45.rmeta: src/main.rs
+
+/home/kali/Terminal-music/terminal_music/target/debug/deps/terminal_music-c3c403ae0d109f45.d: src/main.rs
+
+src/main.rs:
---
+++ terminal_music/target/debug/deps/terminal_music-d48a79ed727f21ce.d
@@ -0,0 +1,5 @@
+/home/kali/Terminal-music/terminal_music/target/debug/deps/terminal_music-d48a79ed727f21ce.rmeta: src/main.rs
+
+/home/kali/Terminal-music/terminal_music/target/debug/deps/terminal_music-d48a79ed727f21ce.d: src/main.rs
+
+src/main.rs:
